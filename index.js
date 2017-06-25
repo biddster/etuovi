5ce2e71111b41369ca79f6d724fbcd678b422c95
biddster <luke.biddell@gmail.com>
2017-06-25 17:31:48 +0100

    Use winston for logging

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const l = require('winston');
+
 const scanners = require('./lib/scanners');
 const fs = require('fs');
 const _ = require('lodash');
@@ -10,9 +12,9 @@ scanners.load();
 
 const config = JSON.parse(fs.readFileSync('example-config.json', 'utf8'));
 const allHosts = _.map(config.hosts, (host) => {
-    console.log(`Scanning host [${host.host}]`);
+    l.info(`Scanning host [${host.host}]`);
     const hostScanners = _.map(host.scanners, (scannerConfig, scannerName) => {
-        console.log(`Running scanner [${scannerName}] against host [${host.host}]`);
+        l.info(`Running scanner [${scannerName}] against host [${host.host}]`);
         let record = {
             host: host.host,
             scanner: scannerName,
@@ -30,18 +32,18 @@ const allHosts = _.map(config.hosts, (host) => {
                 }
                 return record;
             }).finally(() => {
-                console.log(`Finished scanner [${scannerName}] against host [${host.host}]`);
+                l.info(`Finished scanner [${scannerName}] against host [${host.host}]`);
             });
     });
     return Promise.all(hostScanners).then((results) => {
-        console.log(results);
-        console.log(`Scanning host [${host.host}] complete`);
+        // l.info(results);
+        l.info(`Scanning host [${host.host}] complete`);
     });
 });
 
 Promise.all(allHosts).catch((err) => {
-    console.log(err);
+    l.info(err);
 }).finally(() => {
-    console.log(`Exiting with code [${errors.length}]`);
+    l.info(`Exiting with code [${errors.length}]`);
     process.exit(errors.length);
 });
