f0b882d4e9c65152530ea5b91c538a8eaf10f864
biddster <luke.biddell@gmail.com>
2017-07-17 23:53:39 +0100

    Config now has an outputs section for configuring where you want the reports to go. Ie. file, email etc.

--- cmds/new-config.js
+++ cmds/new-config.js
@@ -1,5 +1,5 @@
 const l = require('winston');
-const scanners = require('../lib/scanners');
+const plugins = require('../lib/plugins');
 const fs = require('fs');
 const moment = require('moment');
 const _ = require('lodash');
@@ -12,17 +12,19 @@ module.exports = {
     handler: (argv) => {
         l.info('Making config');
 
+        const loaded = plugins.load();
         const config = {
             hosts: [{
                 host: "www.example.com",
-                scanners: _.mapValues(scanners.load(), scanner => scanner.newConfig())
-            }]
+                scanners: _.mapValues(loaded.scanners, plugin => plugin.newConfig())
+            }],
+            outputs: _.mapValues(loaded.outputs, output => output.newConfig())
         };
 
         l.info(config);
         const file = `etuovi-config-${moment().format('YYYYMMDD-HHmmss')}.json`;
         fs.writeFileSync(file, JSON.stringify(config, null, 4), 'utf8');
-        l.info(`Wrote config file [${file}]`)
+        l.info(`Wrote config file [${file}]`);
         process.exit(0);
     }
 };
